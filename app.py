a46cd0206bfbaa1dc6f222fcde4e42d4164ae82d
ACotto1 <acotto1@kent.edu>
2025-12-01 22:25:38 -0500

    Both planner pages are done!

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from models import db, MealEntry, WorkoutEntry, UserProfile,User
 from sqlalchemy import func
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime, time
 
 app = Flask(__name__)
 
@@ -17,13 +17,198 @@ db.init_app(app)
 def home():
     return render_template("index.html")
 
-@app.route("/fitness")
+@app.route("/fitness", methods=["POST", "GET"])
 def fitness():
-    return render_template("fitness.html")
+    user_id = 1
+    workouts = (
+        db.session.query(WorkoutEntry)
+        .filter(WorkoutEntry.user_id == user_id)
+        .order_by(WorkoutEntry.date.asc())
+        .all()
+    )
 
-@app.route("/meals")
+    today = date.today()
+
+    workout_mins = (
+        db.session.query(
+            func.coalesce(func.sum(WorkoutEntry.duration_minutes), 0)
+        )
+        .filter(WorkoutEntry.user_id == user_id, WorkoutEntry.date == today)
+        .one()
+    )
+
+    dates = []
+    result = []
+    for workout in workouts:
+        if workout.date not in dates:
+            dates.append(workout.date)
+            date_index = dates.index(workout.date)
+            result.insert(date_index, {
+                "day_of_week": workout.date.strftime("%a").capitalize(),
+                "day":workout.date.strftime("%d"),
+                "month": workout.date.strftime("%b"),
+                "workouts": []
+            })
+        date_index = dates.index(workout.date)
+        result[date_index].get("workouts").append(workout)
+
+    current_time = datetime.now().time()
+    greeting = "Good evening."
+    if current_time < time(4, 00):
+        greeting = "Good night."
+    elif current_time < time(12, 00):
+        greeting = "Good morning."
+    elif current_time < time(19, 00):
+        greeting = "Good afternooon"
+
+
+    
+    return render_template("fitness.html", 
+        dates = result,
+        greeting = greeting,
+        workout_mins = workout_mins[0]
+    )
+
+@app.route("/fitness/edit", methods=["POST", "GET"])
+def edit_workout():
+    user_id = 1
+    if request.method == "GET":
+        workout = db.session.query(WorkoutEntry).filter(WorkoutEntry.user_id == user_id, WorkoutEntry.id == request.args.get("workout")).one_or_none()
+        if workout != None:
+            session["edit_query"] = workout.id
+        else:
+            session["edit_query"] = None
+        return render_template("workoutform.html", workout=workout)
+        
+    if request.method == "POST":
+        if (request.form.get("date") == None):
+            old = db.session.query(WorkoutEntry).filter(WorkoutEntry.user_id == user_id, WorkoutEntry.id == session["edit_query"]).one_or_none()
+            
+            if (old != None):
+                db.session.delete(old)
+                db.session.commit()
+                return redirect(url_for("fitness", success = True))
+
+        old = db.session.query(WorkoutEntry).filter(WorkoutEntry.user_id == user_id, WorkoutEntry.id == session["edit_query"]).one_or_none()
+        status = "in progress..."
+        if (old != None):
+            db.session.delete(old)
+            status = old.status
+        
+        db.session.add(WorkoutEntry(
+            user_id=user_id,
+            date= datetime.strptime(request.form.get("date"),"%Y-%m-%d"),
+            workout_name=request.form.get("workout_name"),
+            muscle_group=request.form.get("muscle_group"),
+            sets=request.form.get("sets"),
+            reps=request.form.get("reps"),
+            duration_minutes=request.form.get("duration_minutes"),
+            intensity=request.form.get("intensity"),
+            status=status,
+        ))
+        db.session.commit()
+        return redirect(url_for("fitness", success = True))
+    return render_template("workoutform.html")
+
+@app.route("/meals", methods=["POST", "GET"])
 def meals():
-    return render_template("meals.html")
+    user_id = 1
+    query = (
+        db.session.query(MealEntry)
+        .filter(MealEntry.user_id == user_id)
+        .order_by(MealEntry.date.asc())
+        .all()
+    )
+
+    
+    today = date.today()
+
+    calorie_count = (
+        db.session.query(
+            func.coalesce(func.sum(MealEntry.calories), 0)
+        )
+        .filter(MealEntry.user_id == user_id, MealEntry.date == today)
+        .one()
+    )
+
+    dates = []
+    result = []
+    for meal in query:
+        if meal.date not in dates:
+            dates.append(meal.date)
+            date_index = dates.index(meal.date)
+            result.insert(date_index, {
+                "day_of_week": meal.date.strftime("%a").capitalize(),
+                "day":meal.date.strftime("%d"),
+                "month": meal.date.strftime("%b"),
+                "meals": []
+            })
+        date_index = dates.index(meal.date)
+        result[date_index].get("meals").append(meal)
+
+    current_time = datetime.now().time()
+    greeting = "Good evening."
+    if current_time < time(4, 00):
+        greeting = "Good night."
+    elif current_time < time(12, 00):
+        greeting = "Good morning."
+    elif current_time < time(19, 00):
+        greeting = "Good afternooon"
+
+
+    
+    return render_template("meals.html", 
+        dates = result,
+        greeting = greeting,
+        calorie_count = calorie_count[0]
+    )
+
+# @app.route("/meals/edit", methods=["POST", "GET"])
+# def new_meal():
+#     user_id = 1
+#     if request.method == "GET":
+#         meal = db.session.query(MealEntry).filter(MealEntry.user_id == user_id, MealEntry.id == request.args.get("meal")).one()
+#         return render_template("mealform.html", meal=meal)
+#     return render_template("mealform.html")
+
+@app.route("/meals/edit", methods=["POST", "GET"])
+def new_meal():
+    user_id = 1
+    if request.method == "GET":
+        meal = db.session.query(MealEntry).filter(MealEntry.user_id == user_id, MealEntry.id == request.args.get("meal")).one_or_none()
+        if meal != None:
+            session["edit_query"] = meal.id
+        else:
+            session["edit_query"] = None
+        return render_template("mealform.html", meal=meal)
+        
+    if request.method == "POST":
+        if (request.form.get("date") == None):
+            old = db.session.query(MealEntry).filter(MealEntry.user_id == user_id, MealEntry.id == session["edit_query"]).one_or_none()
+            
+            if (old != None):
+                db.session.delete(old)
+                db.session.commit()
+                return redirect(url_for("meals", success = True))
+
+        old = db.session.query(MealEntry).filter(MealEntry.user_id == user_id, MealEntry.id == session["edit_query"]).one_or_none()
+        
+        if (old != None):
+            db.session.delete(old)
+        
+        db.session.add(MealEntry(
+            user_id=user_id,
+                date=datetime.strptime(request.form.get("date"),"%Y-%m-%d"),
+                meal_type=request.form.get("meal_type"),
+                meal_name=request.form.get("meal_name"),
+                calories=request.form.get("calories"),
+                protein_g=request.form.get("protein_g"),
+                carbs_g=request.form.get("carbs_g"),
+                fats_g=request.form.get("fats_g")
+        ))
+        db.session.commit()
+        return redirect(url_for("meals", success = True))
+    return render_template("mealform.html")
 
 @app.route("/stats")
 def stats():
@@ -275,4 +460,4 @@ def profile_edit():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
